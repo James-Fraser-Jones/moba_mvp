b25be6c2a613ff68ec05085fd8a86edd9fd97f02
James-Fraser-Jones <james@fraser-jones.co.uk>
2024-07-21 16:41:22 +0100

    mvp 2 finished

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ use bevy::{
     prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}
 };
 
+const CAMERA_SPEED: f32 = 500.;
+
+const ARROW_ANG: f32 = 2.*PI/16.;
+const RADIUS: f32 = 56.;
+const RED_HUE: f32 = 0.;
+const GREEN_HUE: f32 = 120.0;
+const BLUE_HUE: f32 = 240.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -15,48 +23,24 @@ fn main() {
 #[derive(Component)]
 struct MainCamera;
 
+#[derive(Component, PartialEq, Default, Copy, Clone)]
+enum Team {
+    #[default] Red,
+    Blue,
+}
+
+#[derive(Bundle, Default)]
+struct UnitBundle {
+    spatial: SpatialBundle,
+    team: Team,
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn((Camera2dBundle::default(), MainCamera));
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Circle { radius: RADIUS })),
-        material: materials.add(Color::hsl(0., 0.7, 0.5)),
-        transform: Transform::from_xyz(
-            0.,
-            0.,
-            0.,
-        ),
-        ..default()
-    });
-
-    // commands.spawn(MaterialMesh2dBundle {
-    //     mesh: Mesh2dHandle(meshes.add(Rectangle::new(50., 100.))),
-    //     material: materials.add(Color::hsl(180., 0.7, 0.5)),
-    //     transform: Transform::from_xyz( 
-    //         100.,
-    //         0.,
-    //         0.,
-    //     ),
-    //     ..default()
-    // });
-
-    const ANG: f32 = 2.*PI/16.;
-    const RADIUS: f32 = 20.;
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Triangle2d::new(Vec2::new(0., RADIUS), Vec2::new(RADIUS*ANG.sin(), -RADIUS*ANG.cos()), Vec2::new(-RADIUS*ANG.sin(), -RADIUS*ANG.cos())))),
-        material: materials.add(Color::hsl(180., 0.7, 0.5)),
-        transform: Transform::from_xyz( 
-            0.,
-            0.,
-            1.,
-        ),
-        ..default()
-    });
-
+    //spawn misc stuff - camera and help text
     commands.spawn(
         TextBundle::from_section("ESC to quit\nW,A,S,D to pan", TextStyle::default())
             .with_style(Style {
@@ -66,29 +50,90 @@ fn setup(
                 ..default()
             }),
     );
+    commands.spawn((Camera2dBundle::default(), MainCamera));
+
+    //initialize mesh and material resources (shared across all units)
+    let bound_mesh_handle = Mesh2dHandle(meshes.add(Circle::new(RADIUS)));
+    let arrow_mesh_handle = Mesh2dHandle(meshes.add(Triangle2d::new(
+        Vec2::new(RADIUS, 0.), 
+        Vec2::new(-RADIUS*ARROW_ANG.cos(), RADIUS*ARROW_ANG.sin()), 
+        Vec2::new(-RADIUS*ARROW_ANG.cos(), -RADIUS*ARROW_ANG.sin())
+    )));
+    let bound_material_handle = materials.add(Color::hsl(GREEN_HUE, 0.5, 0.5));
+    let red_material_handle = materials.add(Color::hsl(RED_HUE, 0.5, 0.5));
+    let blue_material_handle = materials.add(Color::hsl(BLUE_HUE, 0.5, 0.5));
+
+    //initialise units
+    let units = [
+        UnitBundle {
+            spatial: SpatialBundle {
+                transform: Transform::from_xyz(0., 0., 0.).with_rotation(Quat::from_rotation_z(0.)),
+                ..default()
+            },
+            ..default()
+        },
+        UnitBundle {
+            spatial: SpatialBundle {
+                transform: Transform::from_xyz(200., 0., 0.).with_rotation(Quat::from_rotation_z(PI/2.)),
+                ..default()
+            },
+            team: Team::Blue,
+            ..default()
+        },
+        UnitBundle {
+            spatial: SpatialBundle {
+                transform: Transform::from_xyz(0., 200., 0.).with_rotation(Quat::from_rotation_z(3.*PI/4.)),
+                ..default()
+            },
+            team: Team::Blue,
+            ..default()
+        },
+        UnitBundle {
+            spatial: SpatialBundle {
+                transform: Transform::from_xyz(200., 200., 0.).with_rotation(Quat::from_rotation_z(PI)),
+                ..default()
+            },
+            ..default()
+        },
+    ];
+
+    //spawn entities, including adding 2 mesh bundles as child entities of each unit
+    for unit in units.into_iter() {
+        let team = unit.team;
+        commands.spawn(unit).with_children(|parent| {
+            parent.spawn(MaterialMesh2dBundle {
+                mesh: bound_mesh_handle.clone(), //cloning handles to resources is safe
+                material: bound_material_handle.clone(),
+                ..default()
+            });
+            parent.spawn(MaterialMesh2dBundle {
+                mesh: arrow_mesh_handle.clone(),
+                material: if team == Team::Red {red_material_handle.clone()} else {blue_material_handle.clone()},
+                transform: Transform::from_xyz(0., 0., 1.), //ensure arrows are rendered above the bounding circle meshes
+                ..default()
+            });
+        });
+    }
 }
 
-const CAMERA_SPEED: f32 = 500.;
-
 fn move_camera(mut query: Query<&mut Transform, With<MainCamera>>, keyboard: Res<ButtonInput<KeyCode>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        let mut direction: Vec2 = Vec2::ZERO;
-        if keyboard.pressed(KeyCode::KeyA) {
-            direction.x -= 1.;
-        }
-        if keyboard.pressed(KeyCode::KeyD) {
-            direction.x += 1.;
-        }
-        if keyboard.pressed(KeyCode::KeyW) {
-            direction.y += 1.;
-        }
-        if keyboard.pressed(KeyCode::KeyS) {
-            direction.y -= 1.;
-        }
-        direction = direction.normalize_or_zero();
-        direction *= CAMERA_SPEED * time.delta_seconds();
-        transform.translation += direction.extend(0.);
+    let mut transform = query.single_mut(); //okay when entity known to exist and be unique
+    let mut direction: Vec2 = Vec2::ZERO;
+    if keyboard.pressed(KeyCode::KeyA) {
+        direction.x -= 1.;
     }
+    if keyboard.pressed(KeyCode::KeyD) {
+        direction.x += 1.;
+    }
+    if keyboard.pressed(KeyCode::KeyW) {
+        direction.y += 1.;
+    }
+    if keyboard.pressed(KeyCode::KeyS) {
+        direction.y -= 1.;
+    }
+    direction = direction.normalize_or_zero();
+    direction *= CAMERA_SPEED * time.delta_seconds();
+    transform.translation += direction.extend(0.);
 }
 
 fn quit_game(
--- src/mvp.md
+++ src/mvp.md
@@ -1,54 +1,57 @@
 # mvp
 
 ## broad
-* map
-    * square
-    * *no* jungle
-    * 2 *square* lanes: top, bot
-    * diagonal mid lane
-    * river
-    * all lanes and river have some arbitrary width
-    * 4 identical triangles of impassable terrain
-    * 3 turrets per team just off center in each lane (to allow minion pathing)
-    * bases in corners:
-        * heart
-        * two turrets
-    * 180 degree rotational symmetry around midpoint of map for 
-        * turret placement
-        * heart placement
-        * minion paths
-    * linear symmetry also
-* minions
-    * spawning
-    * navigating
-    * following path
-    * collision avoidance
-    * attack moving
-    * 4 melees, 4 ranged
-    * different stats: 
-        * health
-        * attack speed
-        * attack damage
-    * attack speed
-        * windup
-        * register
-        * winddown
-    * projectiles
-    * health bars
-    * dying
-* characters
-    * need 10 (naturally)
-    * effectively powered up minions
-    * have abilities
-    * can "stutter step" i.e. cancel attack winddown with new move command without penalty
-    * no turning speed
+
+- map
+  - square
+  - _no_ jungle
+  - 2 _square_ lanes: top, bot
+  - diagonal mid lane
+  - river
+  - all lanes and river have some arbitrary width
+  - 4 identical triangles of impassable terrain
+  - 3 turrets per team just off center in each lane (to allow minion pathing)
+  - bases in corners:
+    - heart
+    - two turrets
+  - 180 degree rotational symmetry around midpoint of map for
+    - turret placement
+    - heart placement
+    - minion paths
+  - linear symmetry also
+- minions
+  - spawning
+  - navigating
+  - following path
+  - collision avoidance
+  - attack moving
+  - 4 melees, 4 ranged
+  - different stats:
+    - health
+    - attack speed
+    - attack damage
+  - attack speed
+    - windup
+    - register
+    - winddown
+  - projectiles
+  - health bars
+  - dying
+- characters
+  - need 10 (naturally)
+  - effectively powered up minions
+  - have abilities
+  - can "stutter step" i.e. cancel attack winddown with new move command without penalty
+  - no turning speed
 
 ## steps
 
 1. **(DONE)** single unit, displaying
-    * position
-    * radius (colored circle)
-    * orientation (colored triangle within)
 
-2. multiple of these units
+   - position
+   - radius (colored circle)
+   - orientation (colored triangle within)
+
+2. **(DONE)** multiple of these units
+
 3. units having randomised positions, orientations, radiuses
