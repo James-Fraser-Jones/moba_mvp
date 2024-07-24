29e3fd8a5a683cb4f9be268e1f267507f707d652
James-Fraser-Jones <james@fraser-jones.co.uk>
2024-07-24 16:55:03 +0100

    slight tweaks

--- src/game/init.rs
+++ src/game/init.rs
@@ -138,6 +138,7 @@ fn init_waves(mut commands: Commands, handles: Res<Handles>) {
     //wave manager
     commands.insert_resource(WaveManager::new());
 
+    //spawners
     for (lane_pos, lane) in [
         (Vec2::new(-1., 1.), Lane::Top),
         (Vec2::new(0., 0.), Lane::Mid),
@@ -149,10 +150,6 @@ fn init_waves(mut commands: Commands, handles: Res<Handles>) {
         ] {
             let diff = (lane_pos - team_pos).normalize();
             let ang = -diff.angle_between(Vec2::X);
-            println!(
-                "team: {:?}, lane: {:?}, diff: {:?}, ang: {:?}",
-                team, lane, diff, ang
-            );
             let pos = (team_pos + diff * SPAWNER_POS_RADIUS) * MID_LANE * MAP_SIZE;
             spawn_spawner(
                 &mut commands,
@@ -161,24 +158,4 @@ fn init_waves(mut commands: Commands, handles: Res<Handles>) {
             )
         }
     }
-
-    //spawners
-    // let red_start = Vec2::splat(-MID_LANE * MAP_SIZE);
-    // for dir in [NORTH, NORTH_EAST, EAST] {
-    //     let red_pos = red_start + Vec2::from_angle(dir) * SPAWNER_POS_RADIUS * MAP_SIZE;
-    //     spawn_spawner(
-    //         &mut commands,
-    //         &handles,
-    //         SpawnerBundle::new(red_pos.x, red_pos.y, dir, Team::Red, SPAWNER_DELAY),
-    //     )
-    // }
-    // let blue_start = Vec2::splat(MID_LANE * MAP_SIZE);
-    // for dir in [SOUTH, SOUTH_WEST, WEST] {
-    //     let blue_pos = blue_start + Vec2::from_angle(dir) * SPAWNER_POS_RADIUS * MAP_SIZE;
-    //     spawn_spawner(
-    //         &mut commands,
-    //         &handles,
-    //         SpawnerBundle::from_xyrt(blue_pos.x, blue_pos.y, dir, Team::Blue, SPAWNER_DELAY),
-    //     )
-    // }
 }
