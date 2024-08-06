9b8f43eb64e815aadba47ef956b36c6724df0ccb
James-Fraser-Jones <james@fraser-jones.co.uk>
2024-08-06 05:09:47 +0100

    added loading of seperate nodes, added triangulated and extruded as distinct blender nodes

--- src/game/graphics.rs
+++ src/game/graphics.rs
@@ -1,4 +1,4 @@
-use bevy::gltf::GltfMesh;
+use bevy::gltf::{GltfMesh, GltfNode};
 use bevy::{math::Affine2, prelude::*, render::*};
 use std::f32::consts::PI;
 
@@ -16,6 +16,7 @@ struct Gltfs(Vec<Handle<Gltf>>); //handles must be stored somewhere (in a resour
 struct DevMaterials {
     ground: Handle<StandardMaterial>,
     wall: Handle<StandardMaterial>,
+    area: Handle<StandardMaterial>,
 }
 
 fn init(
@@ -60,9 +61,26 @@ fn init(
         uv_transform: Affine2::from_scale(Vec2::splat(10.)),
         ..default()
     });
+    let area_texture: Handle<Image> = server.load_with_settings(
+        "textures/kenney_dev_textures/Green/texture_08.png",
+        |settings: &mut texture::ImageLoaderSettings| {
+            settings.sampler = texture::ImageSampler::Descriptor(texture::ImageSamplerDescriptor {
+                address_mode_u: texture::ImageAddressMode::Repeat,
+                address_mode_v: texture::ImageAddressMode::Repeat,
+                ..default()
+            })
+        },
+    );
+    let area_material = standard_materials.add(StandardMaterial {
+        base_color_texture: Some(area_texture),
+        unlit: true,
+        uv_transform: Affine2::from_scale(Vec2::splat(10.)),
+        ..default()
+    });
     commands.insert_resource(DevMaterials {
         ground: ground_material.clone(),
         wall: wall_material.clone(),
+        area: area_material.clone(),
     });
 
     //add ground plane
@@ -81,6 +99,7 @@ fn handle_gltf_events(
     //for working down through the gltf hierarchy to grab bevy Mesh
     gltf_assets: Res<Assets<Gltf>>,
     gltf_mesh_assets: Res<Assets<GltfMesh>>,
+    gltf_node_assets: Res<Assets<GltfNode>>,
     mut mesh_assets: ResMut<Assets<Mesh>>,
     //for spawning a new entity
     mut commands: Commands,
@@ -93,35 +112,62 @@ fn handle_gltf_events(
                 if path == "models/map.glb".into() {
                     //load map
                     let map = gltf_assets.get(id).unwrap();
-                    //for each individual mesh
-                    for gltf_mesh_handle in map.meshes.iter() {
-                        let gltf_mesh = gltf_mesh_assets.get(gltf_mesh_handle).unwrap();
-                        let gltf_primitive = &gltf_mesh.primitives[0];
-                        //get Handle<Mesh> and Mesh
-                        let mesh_handle = &gltf_primitive.mesh;
-                        let mesh = mesh_assets.get_mut(mesh_handle).unwrap();
-                        //programatically add correct UVs to align with ground plane
-                        let vertices = mesh
-                            .attribute(Mesh::ATTRIBUTE_POSITION)
-                            .unwrap()
-                            .as_float3()
-                            .unwrap();
-                        let mut uvs = vec![Vec2::ZERO; vertices.len()];
-                        for (i, vertex) in vertices.iter().enumerate() {
-                            uvs[i] = (Vec2::new(vertex[0], vertex[1]) + 1000.) / 2000.
-                        }
-                        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-                        //for red and blue team
-                        for rot in [0., PI] {
-                            //spawn copy of the mesh
-                            commands.spawn(PbrBundle {
-                                mesh: mesh_handle.clone(),
-                                material: dev_materials.wall.clone(),
-                                transform: Transform::from_rotation(Quat::from_rotation_z(rot)),
-                                ..default()
-                            });
-                        }
+
+                    //load extruded
+                    let node_handle = map.named_nodes.get("RedExtruded").unwrap();
+                    let node = gltf_node_assets.get(node_handle).unwrap();
+                    let gltf_mesh_handle = node.mesh.as_ref().unwrap();
+                    let gltf_mesh = gltf_mesh_assets.get(gltf_mesh_handle).unwrap();
+                    let gltf_primitive = &gltf_mesh.primitives[0];
+                    let mesh_handle = &gltf_primitive.mesh;
+                    let mesh = mesh_assets.get_mut(mesh_handle).unwrap();
+                    let vertices = mesh
+                        .attribute(Mesh::ATTRIBUTE_POSITION)
+                        .unwrap()
+                        .as_float3()
+                        .unwrap();
+                    let mut uvs = vec![Vec2::ZERO; vertices.len()];
+                    for (i, vertex) in vertices.iter().enumerate() {
+                        uvs[i] = (Vec2::new(vertex[0], vertex[1]) + 1000.) / 2000.
                     }
+                    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+                    for rot in [0., PI] {
+                        commands.spawn(PbrBundle {
+                            mesh: mesh_handle.clone(),
+                            material: dev_materials.wall.clone(),
+                            transform: Transform::from_rotation(Quat::from_rotation_z(rot)),
+                            ..default()
+                        });
+                    }
+
+                    // //load triangulated
+                    // let node_handle = map.named_nodes.get("RedTriangulated").unwrap();
+                    // let node = gltf_node_assets.get(node_handle).unwrap();
+                    // for child in node.children.iter() {
+                    //     let gltf_mesh_handle = child.mesh.as_ref().unwrap();
+                    //     let gltf_mesh = gltf_mesh_assets.get(gltf_mesh_handle).unwrap();
+                    //     let gltf_primitive = &gltf_mesh.primitives[0];
+                    //     let mesh_handle = &gltf_primitive.mesh;
+                    //     let mesh = mesh_assets.get_mut(mesh_handle).unwrap();
+                    //     let vertices = mesh
+                    //         .attribute(Mesh::ATTRIBUTE_POSITION)
+                    //         .unwrap()
+                    //         .as_float3()
+                    //         .unwrap();
+                    //     let mut uvs = vec![Vec2::ZERO; vertices.len()];
+                    //     for (i, vertex) in vertices.iter().enumerate() {
+                    //         uvs[i] = (Vec2::new(vertex[0], vertex[1]) + 1000.) / 2000.
+                    //     }
+                    //     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+                    //     for rot in [0., PI] {
+                    //         commands.spawn(PbrBundle {
+                    //             mesh: mesh_handle.clone(),
+                    //             material: dev_materials.area.clone(),
+                    //             transform: Transform::from_rotation(Quat::from_rotation_z(rot)),
+                    //             ..default()
+                    //         });
+                    //     }
+                    // }
                 }
             }
             _ => {}
