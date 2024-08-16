7ef60d9fcb8337461e4144b1dc7e97c4ed39795e
James-Fraser-Jones <james@fraser-jones.co.uk>
2024-08-16 17:53:58 +0100

    added app icon

--- src/game/os.rs
+++ src/game/os.rs
@@ -3,9 +3,10 @@
 //quitting the game
 //saving/loading assets to/from the filesystem
 
-use bevy::{prelude::*, window::*};
+use bevy::{prelude::*, window::*, winit::WinitWindows};
 use std::collections::HashMap;
 use std::sync::LazyLock;
+use winit::window::Icon;
 
 pub struct OSPlugin;
 impl Plugin for OSPlugin {
@@ -14,6 +15,22 @@ impl Plugin for OSPlugin {
         app.add_systems(Update, update);
     }
 }
+
+const GAME_ICON: &str = "assets/textures/moba_icon.png";
+const GAME_NAME: &str = "Moba MVP";
+static WINDOW: LazyLock<Window> = LazyLock::new(|| Window {
+    title: GAME_NAME.to_string(),
+    name: Some(GAME_NAME.to_string()),
+    position: WindowPosition::At(IVec2::new(0, 0)),
+    resolution: WindowResolution::new(1920., 1080.),
+    mode: WindowMode::Windowed,
+    cursor: Cursor {
+        grab_mode: CursorGrabMode::None,
+        visible: false,
+        ..default()
+    },
+    ..default()
+});
 
 #[derive(Resource)]
 pub struct HandleMap<K: Eq + std::hash::Hash, A: Asset>(pub HashMap<K, Handle<A>>);
@@ -46,24 +63,23 @@ impl<K: Eq + std::hash::Hash, A: Asset> Default for HandleMap<K, A> {
     }
 }
 
-const GAME_NAME: &str = "Moba MVP";
-static WINDOW: LazyLock<Window> = LazyLock::new(|| Window {
-    title: GAME_NAME.to_string(),
-    name: Some(GAME_NAME.to_string()),
-    position: WindowPosition::At(IVec2::new(0, 0)),
-    resolution: WindowResolution::new(1920., 1080.),
-    mode: WindowMode::Windowed,
-    cursor: Cursor {
-        grab_mode: CursorGrabMode::None,
-        visible: false,
-        ..default()
-    },
-    ..default()
-});
-
-fn init(mut window_query: Query<&mut Window, With<PrimaryWindow>>) {
+fn init(mut window_query: Query<&mut Window, With<PrimaryWindow>>, windows: NonSend<WinitWindows>) {
+    //set window settings
     let mut window = window_query.single_mut();
     *window = WINDOW.clone();
+    //set window icon
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(GAME_ICON)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+    for window in windows.windows.values() {
+        window.set_window_icon(Some(icon.clone()));
+    }
 }
 
 fn update(keyboard: Res<ButtonInput<KeyCode>>, mut writer: EventWriter<AppExit>) {
